ef2c0559f7d453ed24f033c7389f7821df009e52
itsmeabhinraj <abhinraj1038@gmail.com>
2025-02-28 02:32:31 +0530

    2nd web 2nd day

--- custom_shop_visibilty/controller/main.py
+++ custom_shop_visibilty/controller/main.py
@@ -1,6 +1,8 @@
 from odoo import http
 from odoo.http import request, route
+from odoo.addons.website_sale.controllers import main
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.tools import lazy
 
 class WebsiteSaleInherit(WebsiteSale):
 
@@ -11,21 +13,44 @@ class WebsiteSaleInherit(WebsiteSale):
         '/shop/category/<model("product.public.category"):category>/page/<int:page>',
     ], type='http', auth="public", website=True)
     def shop(self, page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post):
-        super(WebsiteSaleInherit, self).shop(page=0, category=None, search='', ppg=False, **post)
+        res = super(WebsiteSaleInherit, self).shop(page, category, search, min_price, max_price, ppg, **post)
         user = request.env.user
-        # partner_id = request.env.user.partner_id
-        partner = user.partner_id
         print('user',user)
-        print('partner',partner)
-        xxx = partner.allowed_products_ids
-        print('user',xxx)
-        # for record in
+        # apply conditions for filter products
         if user.partner_id.allowed_products_ids:
             print("reached")
-            products = request.env['product.product'].search([('id','=',user.partner_id.allowed_products_ids.ids)])
-            print('products',products)
+            products = request.env['product.template'].search([('id','in',user.partner_id.allowed_products_ids.ids)])
+            print('products demo',products)
         else:
-            products = request.env['product.product'].search([])
-        # allowed_products = user.partner_id.allowed_product_ids
-        # print("Inherited ....", res)
-        return request.render("website_sale.products",{'values':{'products':products}})
+            products = request.env['product.template'].search([])
+            print('products1', products)
+        res.qcontext['products'] = products
+        ppg = res.qcontext['ppg']
+        ppr = res.qcontext['ppr']
+        res.qcontext['bins'] = lazy(lambda: main.TableCompute().process(products, ppg, ppr))
+        return res
+        # # res.qcontext.update({'products':products})
+        # print('bins',res.qcontext['bins'])
+        # # res.qcontext['bins'] = products
+        # print('bins', res.qcontext['bins'])
+        # return res
+
+    # res.qcontext['category'] = category
+    # res.qcontext['attributes'] = attributes
+    # res.qcontext['search'] = search
+    # res.qcontext['pager'] = pager
+    # res.qcontext['search_product'] = search_product
+    # res.qcontext['products_prices'] = products_prices
+
+    # categ = res.qcontext['categories']
+    # print(categ)
+    # res.qcontext.update({'categories': categ})
+
+    # website = request.env['website'].get_current_website()
+    # fuzzy_search_term, product_count, search_product = self._shop_lookup_products(attrib_set, options, post, search,
+    #                                                                              website)
+
+    # res.qcontext['pager'] = website.pager(url=url, total=product_count, page=page, step=ppg, scope=5, url_args=post)
+    # if category:
+    #     categ = res.qcontext['categories']
+    #     res.qcontext.update({'categories': categ})
--- custom_shop_visibilty/models/res_partner.py
+++ custom_shop_visibilty/models/res_partner.py
@@ -4,5 +4,5 @@ class ResPartner(models.Model):
     '''inheriting model res.user to add a allowed product field'''
     _inherit = 'res.partner'
 
-    allowed_products_ids = fields.Many2many('product.product',string="Allowed products")
+    allowed_products_ids = fields.Many2many('product.template',string="Allowed products")
     demo_field = fields.Boolean('Boolean')
