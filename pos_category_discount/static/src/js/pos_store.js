68b5719047c3d7e57a46b51fc3aaaf2c0a458159
itsmeabhinraj <abhinraj1038@gmail.com>
2025-03-12 00:56:16 +0530

    POS category wise disc final stage

--- pos_category_discount/static/src/js/pos_store.js
+++ pos_category_discount/static/src/js/pos_store.js
@@ -11,38 +11,20 @@ patch(PosStore.prototype, {
         var categoryDiscounts = {};
         var totalDiscountedPrice = 0
         var totalOriginalPrice = 0
+        var PosCategory = this.models["pos.category"].records || [];
         //iterate through orderlines
         orderlines.forEach((line) => {
-//            var categories = [];
-//            line.product_id.pos_categ_ids.forEach((category) => {
-//                categories.push(category.name);
-//            });
-//            console.log('categoried',categories);
-            console.log('orderlines',orderlines);
             var product = line.get_product();
-            console.log('product',product)
-            //product discount - in %
             var line_discount = line.get_discount();
-            console.log("line_discount",line_discount);
-
             var unit_qty = line.get_quantity();
-            console.log('unit',unit_qty)
-
             var price_without_discount = line.getUnitDisplayPriceBeforeDiscount() * unit_qty;
-            console.log('price_without_disco',price_without_discount);
-
             var price_subtotal = line.price_subtotal;
-            console.log('price_subtotal',price_subtotal);
-
             var product_categories = product.pos_categ_ids;
-            console.log("product_categories",product_categories);
             //final price of product after tax (if discounts appiled , then it also
             var final_orderline_price = price_without_discount - (price_without_discount * line_discount/100)
-            console.log("discounted_price",final_orderline_price)
             //iterate through category
             product_categories.forEach((category_id) => {
                 var category_id = category_id.id;
-                console.log("categ name",category_id)
                 // If the category doesn't exist in the dictionary, initialize it
                 if (!categoryDiscounts[category_id]) {
                     categoryDiscounts[category_id] = {
@@ -54,181 +36,51 @@ patch(PosStore.prototype, {
                  // Add the total cost and actual price of this product to the corresponding category
                 categoryDiscounts[category_id].total_discounted_price += final_orderline_price;
                 categoryDiscounts[category_id].total_original_price += price_without_discount;
-
-                console.log("categoryDiscounts",categoryDiscounts)
-                // Object { 1: {…}, 2: {…}, 3: {…} }
-                console.log("categoryDiscounts",categoryDiscounts[category_id])
-                // Object { category_name: undefined, total_discounted_price: 207.86999999999998,
-                // total_original_price: 277.15999999999997 }
             });
             for (var category_id in categoryDiscounts) {
                 const CategoryId = categoryDiscounts[category_id]
                 totalDiscountedPrice = CategoryId.total_discounted_price
                 totalOriginalPrice = CategoryId.total_original_price
-                const category = CategoryId.category_name;
-                console.log("category",category)
-
+                var category = CategoryId.category_name;
                 const discountType = category.discount_type;
-                console.log('discountType',discountType)
-                var PosCategory = this.models["pos.category"].getFirst();
-                console.log("PosCategory",PosCategory)
-//                var heloo = PosCategory.discount_type
-//                console.log("heloo",heloo)
+                var PosCategory = this.models["pos.category"].getAll();
+                const posCategoryArray = PosCategory;
+                for (let i = 0; i < posCategoryArray.length; i++) {
+                    var AllCategory = posCategoryArray[i];
+                    var discount_type = AllCategory.discount_type
+                    var FixedDiscountLimit = AllCategory.fixed_discount
+                    var PercentageDiscountLimit = AllCategory.percent_discount
+                    if (category == AllCategory.id) {
+                        if (discount_type == 'fixed') {
+                            var AllowedDiscount = totalOriginalPrice - FixedDiscountLimit
+                            if (AllowedDiscount > totalDiscountedPrice) {
+                                this.dialog.add(AlertDialog, {
+                                    title: ("Alert!"),
+                                    body: ("Category discount exceeded the limit."),
+                                });
+                                return;
+                            }
+                            else {
+                                super.pay();
+                            }
+                        }
+                        else if (discount_type == 'percentage') {
+                            var AllowedDiscount = totalOriginalPrice - (totalOriginalPrice * PercentageDiscountLimit)/100
+                            if (AllowedDiscount > totalDiscountedPrice) {
+                                this.dialog.add(AlertDialog, {
+                                    title: ("Alert!"),
+                                    body: ("Category discount exceeded the limit."),
+                                });
+                                return;
+                            }
+                            else {
+                                super.pay();
+                            }
+                        }
+
+                    };
+                }
             }
-
         });
-//        iterate over categoryDiscount dict to access total discount price and original price
-//        then compare it with allowed price
-
-//        for (var category_id in categoryDiscounts) {
-//            const CategoryId = categoryDiscounts[category_id]
-////            console.log("current_discount_total",totalDiscountedPrice)
-//            totalDiscountedPrice = CategoryId.total_discounted_price
-//            totalOriginalPrice = CategoryId.total_original_price
-//            const category = CategoryId.category_name;
-//            console.log("category",category)
-//
-//            var PosCategory = this.models["pos.category"].pos_categ_ids;
-//            console.log("PosCategory",PosCategory)
-//            var FixedDiscountLimit = this.models["pos.category"].getFirst().fixed_discount
-//            console.log("PosCategory1",FixedDiscountLimit)
-//            console.log("this",this)
-//            const discountType = product_categories.discount_type;
-//            console.log("discountType",discountType)
-//            const FixedDiscountLimit = product_categories.fixed_discount;
-//            console.log("FixedDiscountLimit",FixedDiscountLimit)
-//            const PercentageDiscountLimit = product_categories.percent_discount;
-//            console.log("PercentageDiscountLimit",PercentageDiscountLimit)
-//
-//
-//            console.log("total_discount_total",totalDiscountedPrice)
-//            console.log("current_pricet_total",totalOriginalPrice)
-//            if (discount_type == 'fixed') {
-//                var AllowedDiscount = totalOriginalPrice - FixedDiscountLimit
-//                console.log("AllowedDiscount",AllowedDiscount)
-//                if (AllowedDiscount < totalDiscountedPrice) {
-//                    console.log('error')
-//                };
-//            }
-//            else if (discount_type == 'percent_discount') {
-//                var AllowedDiscount = totalOriginalPrice - (totalOriginalPrice * PercentageDiscountLimit)/100
-//                console.log("AllowedDiscount",AllowedDiscount)
-//                if (AllowedDiscount < totalDiscountedPrice) {
-//                    console.log('error')
-//                };
-//            }
-
-//        }
-        return super.pay();
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-//
-//        var orderlines = this.get_order().get_orderlines();
-//        var categoryDiscounts = {}; // Store category-wise discount details
-//
-//        // Loop through order lines and calculate discount per category
-//        orderlines.forEach((line) => {
-//            var product = line.get_product();
-//            var line_discount = line.get_discount();
-//            var line_price = line.price_subtotal;
-//            var product_categories = product.pos_categ_ids;
-//
-//            product_categories.forEach((categ_id) => {
-//                // Fetch category discount settings directly from pos.category
-//                var categ_config = this.pos.categories_by_id[categ_id];
-//                if (categ_config && categ_config.has_discount_limit) {
-//                    var discount_type = categ_config.discount_type; // 'fixed' or 'percentage'
-//                    var discount_limit = categ_config.discount_limit; // Limit value
-//
-//                    // Initialize category tracking if not set
-//                    if (!categoryDiscounts[categ_id]) {
-//                        categoryDiscounts[categ_id] = {
-//                            discount_amount: 0,
-//                            total_unit_price: 0
-//                        };
-//                    }
-//
-//                    // Compute applied discount for the category
-//                    var applied_discount = (line_price * line_discount) / 100;
-//                    categoryDiscounts[categ_id].discount_amount += applied_discount;
-//                    categoryDiscounts[categ_id].total_unit_price += line_price / ((100 - line_discount) / 100);
-//                }
-//            });
-//        });
-//
-//        // Validate discount limits per category
-//        for (const categ_id in categoryDiscounts) {
-//            var categ_config = this.pos.categories_by_id[categ_id];
-//            var discount_type = categ_config.discount_type;
-//            var discount_limit = categ_config.discount_limit;
-//            var applied_discount = categoryDiscounts[categ_id].discount_amount;
-//            var total_unit_price = categoryDiscounts[categ_id].total_unit_price;
-//
-//            if (discount_type === "percentage") {
-//            elif (category_id in categoryDiscounts){
-//                };      var applied_discount_percent = (applied_discount / total_unit_price) * 100;
-//                if (applied_discount_percent > discount_limit) {
-//                    this.dialog.add(AlertDialog, {
-//                        title: _t("Warning"),
-//                        body: _t(The applied discount (${applied_discount_percent.toFixed(2)}%) exceeds the allowed limit (${discount_limit}%) for this category.),
-//                    });
-//                    return;
-//                }
-//            } else if (discount_type === "fixed") {
-//                var discounted_total = total_unit_price - applied_discount;
-//                if (discounted_total < discount_limit) {
-//                    this.dialog.add(AlertDialog, {
-//                        title: _t("Warning"),
-//                        body: _t(The applied discount amount exceeds the allowed fixed limit for this category.),
-//                    });
-//                    return;
-//                }
-//            }
-//        }
-
-        // Proceed with payment if discount limits are not exceeded
-//        return super.pay();
-//    }
-//});
-
-
-
-
-
--- pos_product_brand/static/src/js/sample.js
+++ pos_product_brand/static/src/js/sample.js
@@ -1,135 +1,141 @@
-import { _t } from "@web/core/l10n/translation";
+/** @odoo-module **/
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { patch } from "@web/core/utils/patch";
 import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 
-patch(PosStore.prototype, {
-    async pay() {
-        console.log(this.get_order());
-        const order = this.get_order();
-        const orderlines = order.get_orderlines();
-        const categoryDiscounts = {}; // Store category-wise discounts
-
-        for (const line of orderlines) {
-            const product = line.get_product();
-            const lineDiscount = line.get_discount();
-            const productCategories = product.pos_categ_ids;
-            const originalLinePrice = line.get_price_with_tax();
-            const discountedLinePrice = line.price_subtotal;
-            const actualDiscount = originalLinePrice - discountedLinePrice;
-
-            for (const categoryId of productCategories) {
-                const categoryObj = this.env.pos.db.get_category_by_id(categoryId);
-                if (categoryObj && categoryObj.discount_limit && categoryObj.discount_type) {
-                    if (!categoryDiscounts[categoryId]) {
-                        categoryDiscounts[categoryId] = {
-                            category: categoryObj,
-                            totalDiscount: 0,
-                            totalOriginal: 0,
-                        };
-                    }
-                    categoryDiscounts[categoryId].totalDiscount += actualDiscount;
-                    categoryDiscounts[categoryId].totalOriginal += originalLinePrice;
-                }
-            }
-        }
-
-        for (const categoryId in categoryDiscounts) {
-            const categoryData = categoryDiscounts[categoryId];
-            const category = categoryData.category;
-            const totalDiscount = categoryData.totalDiscount;
-            const totalOriginal = categoryData.totalOriginal;
-            const discountLimit = category.discount_limit;
-            const discountType = category.discount_type;
-
-            if (discountType === 'percentage') {
-                const maxAllowedDiscount = (totalOriginal * discountLimit) / 100;
-
-                if (totalDiscount > maxAllowedDiscount) {
-                    this.dialog.add(AlertDialog, {
-                        title: _t("Warning"),
-                        body: _t(Total discount (${totalDiscount.toFixed(2)}) exceeds the allowed limit (${discountLimit}%) for category: ${category.name}),
-                    });
-                    return; // Prevent payment
-                }
-            } else if (discountType === 'fixed') {
-                if (totalDiscount > discountLimit) {
-                    this.dialog.add(AlertDialog, {
-                        title: _t("Warning"),
-                        body: _t(Total discount (${totalDiscount.toFixed(2)}) exceeds the allowed limit (${discountLimit}) for category: ${category.name}),
-                    });
-                    return; // Prevent payment
-                }
-            }
-        }
-
-        return super.pay();
-    },
-});
-
-
-import { PosStore } from "@point_of_sale/app/store/pos_store";
-import { patch } from "@web/core/utils/patch";
-import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
-
-// Patch the PosStore to modify the payment logic
 patch(PosStore.prototype, {
     async pay() {
         var orderlines = this.get_order().get_orderlines();
+//        var product = line.get_product();
+//        var product_categories = product.pos_categ_ids;
         var categoryDiscounts = {};
-        var totalDiscountedPrice = 0;
-        var totalOriginalPrice = 0;
-
-        // Iterate through order lines to calculate discounts and prices
+        var totalDiscountedPrice = 0
+        var totalOriginalPrice = 0
+        var PosCategory = this.models["pos.category"].records || [];
+        console.log("PosCategory",PosCategory)
+        //iterate through orderlines
         orderlines.forEach((line) => {
-            var categories = [];
-            // Access categories of the product
-            line.product_id.pos_categ_ids.forEach((category) => {
-                categories.push(category.name);  // Push category names into array
-            });
-            console.log('Categories:', categories);
-            console.log('Order lines:', orderlines);
+//            var categories = [];
+//            line.product_id.pos_categ_ids.forEach((category) => {
+//                categories.push(category.name);
+//            });
+//            console.log('categoried',categories);
+            console.log('orderlines',orderlines);
             var product = line.get_product();
-            console.log('Product:', product);
-
-            // Get product discount - as percentage
+            console.log('product',product)
+            //product discount - in %
             var line_discount = line.get_discount();
-            console.log("Line discount:", line_discount);
+            console.log("line_discount",line_discount);
 
             var unit_qty = line.get_quantity();
-            console.log('Quantity:', unit_qty);
+            console.log('unit',unit_qty)
 
-            // Price without discount
             var price_without_discount = line.getUnitDisplayPriceBeforeDiscount() * unit_qty;
-            console.log('Price without discount:', price_without_discount);
+            console.log('price_without_disco',price_without_discount);
 
-            // Subtotal price of the order line
             var price_subtotal = line.price_subtotal;
-            console.log('Price Subtotal:', price_subtotal);
+            console.log('price_subtotal',price_subtotal);
 
-            // Categories related to the product
             var product_categories = product.pos_categ_ids;
-            console.log("Product Categories:", product_categories);
-
-            // Final price of the order line after discount
-            var final_orderline_price = price_without_discount - (price_without_discount * line_discount / 100);
-            console.log("Discounted Price:", final_orderline_price);
-
-            // Iterate through product categories
-            product_categories.forEach((category) => {
-                var category_id = category.id;  // Access the category ID
-                var category_name = category.name;  // Access the category name
-                console.log("Category ID:", category_id);
-                console.log("Category Name:", category_name);
-
+            console.log("product_categories",product_categories);
+            //final price of product after tax (if discounts appiled , then it also
+            var final_orderline_price = price_without_discount - (price_without_discount * line_discount/100)
+            console.log("discounted_price",final_orderline_price)
+            //iterate through category
+            product_categories.forEach((category_id) => {
+                var category_id = category_id.id;
+                console.log("categ name",category_id)
                 // If the category doesn't exist in the dictionary, initialize it
                 if (!categoryDiscounts[category_id]) {
                     categoryDiscounts[category_id] = {
-                        category_name: category_name,  // Store the category name
+                        category_name: category_id,
                         total_discounted_price: 0,
-                        total_original_price: 0,
+                        total_original_price:0
                     };
                 }
+                 // Add the total cost and actual price of this product to the corresponding category
+                categoryDiscounts[category_id].total_discounted_price += final_orderline_price;
+                categoryDiscounts[category_id].total_original_price += price_without_discount;
 
-                // Add the discounted price and original price to the corresponding category
-                categoryDiscounts[category_id].total_discounted_price +=
+                console.log("categoryDiscounts",categoryDiscounts)
+                // Object { 1: {…}, 2: {…}, 3: {…} }
+                console.log("categoryDiscounts",categoryDiscounts[category_id])
+                // Object { category_name: undefined, total_discounted_price: 207.86999999999998,
+                // total_original_price: 277.15999999999997 }
+            });
+            for (var category_id in categoryDiscounts) {
+                const CategoryId = categoryDiscounts[category_id]
+                totalDiscountedPrice = CategoryId.total_discounted_price
+                totalOriginalPrice = CategoryId.total_original_price
+                var category = CategoryId.category_name;
+                console.log("category",category)
+
+                const discountType = category.discount_type;
+                console.log('discountType',discountType)
+                var PosCategory = this.models["pos.category"].getAll();
+                console.log("PosCategory",PosCategory)
+//                let poscate = PosCategory.find(cat => cat.id == category_id);
+                var heloo = PosCategory.discount_type
+                console.log("heloo",heloo)
+                var demo = PosCategory[0]
+                console.log("demo",demo)
+                console.log("demo",demo.percent_discount)
+                console.log("demo",demo.discount_type)
+
+                const posCategoryArray = PosCategory; // Your array-like structure
+
+                for (let i = 0; i < posCategoryArray.length; i++) {
+                    var AllCategory = posCategoryArray[i];
+                    console.log("xxx",AllCategory); // Access each PosCategory object here
+                    console.log("xxdiscount",AllCategory.discount_type)
+                    var discount_type = AllCategory.discount_type
+                    var FixedDiscountLimit = AllCategory.fixed_discount
+                    var PercentageDiscountLimit = AllCategory.percent_discount
+                    console.log("FixedDiscountLimit",FixedDiscountLimit)
+                    console.log("PercentageDiscountLimit",PercentageDiscountLimit)
+                    if (category == AllCategory.id) {
+                        console.log('existed category',category)
+                        console.log('existed category2',AllCategory.id)
+                        if (discount_type == 'fixed') {
+                            var AllowedDiscount = totalOriginalPrice - FixedDiscountLimit
+                            console.log("AllowedDiscount",AllowedDiscount)
+                            console.log("totalOriginalPrice",totalOriginalPrice)
+                            if (AllowedDiscount > totalDiscountedPrice) {
+                                console.log('error')
+                                this.dialog.add(AlertDialog, {
+                                    title: ("Alert!"),
+                                    body: ("Category discount exceeded the limit."),
+                                });
+                                return;
+                            }
+                            else {
+                                super.pay();
+                            }
+                            console.log("totalDiscountedPrice",totalDiscountedPrice)
+                            console.log("AllowedDiscount",AllowedDiscount)
+                        }
+                        else if (discount_type == 'percentage') {
+                            var AllowedDiscount = totalOriginalPrice - (totalOriginalPrice * PercentageDiscountLimit)/100
+                            console.log("AllowedDiscount",AllowedDiscount);
+                            if (AllowedDiscount > totalDiscountedPrice) {
+                                console.log('error');
+                                this.dialog.add(AlertDialog, {
+                                    title: ("Alert!"),
+                                    body: ("Category discount exceeded the limit."),
+                                });
+                                return;
+                            }
+                            else {
+                                super.pay();
+                            }
+                            console.log("AllowedDiscount",AllowedDiscount);
+                            console.log("totalDiscountedPrice",totalDiscountedPrice);
+
+                        }
+
+                    };
+                }
+            }
+        });
+    }
+});
